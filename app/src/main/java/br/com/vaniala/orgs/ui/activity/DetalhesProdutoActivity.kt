3f6bb880a1e2488afc7a09838d9152c9484f11e9
Vania <vania.almeida28@hotmail.com>
2023-02-10 21:39:53 -0300

    criando menu

--- app/src/main/java/br/com/vaniala/orgs/ui/activity/DetalhesProdutoActivity.kt
+++ app/src/main/java/br/com/vaniala/orgs/ui/activity/DetalhesProdutoActivity.kt
@@ -2,7 +2,10 @@ package br.com.vaniala.orgs.ui.activity
 
 import android.os.Build
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import br.com.vaniala.orgs.R
 import br.com.vaniala.orgs.databinding.ActivityDetalhesProdutoBinding
 import br.com.vaniala.orgs.extensions.formataParaMoedaBrasileira
 import br.com.vaniala.orgs.extensions.tentaCarregarImagem
@@ -23,6 +26,20 @@ class DetalhesProdutoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         tentaCarregarProduto()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_detalhes_produto, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_detalhes_produto_editar -> {
+            }
+            R.id.menu_detalhes_produto_remover -> {}
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun tentaCarregarProduto() {
